8249857d0139a8f92562fd4bab51c7369820d07c
Danny <d.markov94@gmail.com>
2024-12-02 17:14:41 +0200

    Upgraded storybook to v8.4.6

--- cart-react/.storybook/main.cjs
+++ cart-react/.storybook/main.cjs
@@ -1,15 +1,32 @@
+const { dirname, join } = require("path");
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+
+  addons: [
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-webpack5-compiler-babel"),
+    getAbsolutePath("@chromatic-com/storybook"),
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "disableTelemetry": true
+
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {}
+  },
+
+  core: {
+    disableTelemetry: true,
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
   }
+};
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
 }
--- cart-react/.storybook/preview.cjs
+++ cart-react/.storybook/preview.cjs
@@ -6,4 +6,5 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+export const tags = ["autodocs"];
